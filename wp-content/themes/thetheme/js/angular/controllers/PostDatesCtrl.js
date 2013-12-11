f18aced370f7a10b10441ca1f81b52e965331df0
Benjamin Scheib <ben@discoverhawaiitours.com>
2013-12-10 15:47:33 -1000

    put my form in the right place for members

--- wp-content/themes/thetheme/js/angular/controllers/PostDatesCtrl.js
+++ wp-content/themes/thetheme/js/angular/controllers/PostDatesCtrl.js
@@ -1,5 +1,24 @@
 function PostDatesCtrl($scope, $rootScope, $http, $routeParams, $filter, $location){ 
-  
+  var member1 = {"name": "Ben Scheib", "id": 1234};
+  var member2 = {"name": "Denis Crowder", "id": 12345};
+  var member3 = {"name": "Leo Malagan", "id": 123456};
+  $scope.companies = [{"name": "Company1", "id": 12345, "members": [member1, member2]},
+  					  {"name": "Company2", "id": 12345, "members": [member3]}];
+  $scope.postDate = {"parcels": []};
+  $scope.addParcel = function(){
+	$scope.postDate.parcels.push({});
+  }
+  $scope.postDateValid = function(){
+	$valid = false;
+	try{
+		var $postDate = new Date($scope.postDate.date);
+		if($postDate != 'Invalid Date'){
+			$valid = true;
+		}
+	}catch(Error){
+	}
+	return $valid;
+  }
 }
 PostDatesCtrl.$inject = ['$scope', '$rootScope', '$http', '$routeParams', '$filter', '$location'];
 app.controller('PostDatesCtrl', PostDatesCtrl);
--- wp-content/themes/thetheme/members.php
+++ wp-content/themes/thetheme/members.php
@@ -8,10 +8,3 @@ $warp = require(__DIR__.'/warp.php');
 // load main template file
 echo $warp['template']->render('theme');
 ?>
-
-<div ng-controller="PostDatesCtrl" ng-cloak>
-	<div ng-form="members-form">
-		<input name="postal-date" type="text" ng-model="post_date.date"/>
-		<input type="submit" />
-	</div>
-</div>
